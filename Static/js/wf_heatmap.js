435968b9707ef50c8a6784b1aa7f5936b6c46b71
Alberto Gonzalez <78628287+dalismo@users.noreply.github.com>
2021-07-24 13:27:05 -0400

    creating static folders and added code for heatmap

---
+++ Static/js/wf_heatmap.js
@@ -0,0 +1,9 @@
+var myMap = L.map("map", {
+    center: [39.8283, -98.5795],
+    zoom: 5
+  });
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
