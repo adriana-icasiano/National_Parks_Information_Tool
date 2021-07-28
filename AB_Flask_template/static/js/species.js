d985513f5555908e040052208b118b8fe8bad177
Abayomi Olujobi <bay.olu87@gmail.com>
2021-07-27 23:34:28 -0400

    Update dropdown to dynamically display animal order list

--- AB_Flask_template/static/js/species.js
+++ AB_Flask_template/static/js/species.js
@@ -20,16 +20,15 @@ function init() {
         parkNames.forEach(function (park) {
             dropdown1.append("option").text(park).property("value");
         });
-        
+
         let category_name = ""
-        
+
         d3.select("select").on("change", function (i) {
-            category_name = d3.select("#speciesDropdown").node().value;
+            let category_name = d3.select("#speciesDropdown").node().value;
             // console.log(category_name);
         });
 
         parkFilter(parkNames[0], category_name);
-        // animalCategory(category_name);
 
     });
 }
@@ -38,8 +37,44 @@ function parkFilter(park_name, category_name) {
 
     d3.json("/species").then(data => {
 
-        let result = data.filter(obj => obj.park_name === park_name && obj.category_name === category_name);
+        let result = data.filter(obj => obj.park_name === park_name
+            && obj.category_name === category_name);
         console.log(result);
+
+        let lookup = {};
+        let parkAnimalOrders = result;
+        let animalOrder = [];
+
+        for (let parkAnimalOrder, i = 0; parkAnimalOrder = parkAnimalOrders[i++];) {
+            let orderName = parkAnimalOrder.order;
+
+            if (!(orderName in lookup)) {
+                lookup[orderName] = 1;
+                animalOrder.push(orderName)
+            };
+        };
+        // console.log(animalOrder);
+        for (let i = 0; i < animalOrder.length; i++) {
+            console.log(animalOrder[i]);
+
+            let dropdown3 = d3.select("#orderDropdown");
+            dropdown3.html("");
+            animalOrder.forEach(function (order) {
+                dropdown3.append("option").text(order).property("value");
+
+                // let orderObj = document.getElementById("orderDropdown");
+                // console.log(orderObj);
+                // let ordr = orderObj.querySelectorAll("option");
+                // if (ordr === result.order){
+                //     return result;
+                // }
+            });
+
+
+
+        }
+
+        // document.getElementById("orderPrint").innerHTML = animalOrder.join("<br>");
 
     });
 }
@@ -50,90 +85,8 @@ function optionChanged(newCategory_name) {
     let park_name = d3.select("#selDataset").node().value;
     console.log(park_name);
     parkFilter(park_name, newCategory_name);
+    console.log(newCategory_name);
+
 }
 
 init();
-
-// Mammals in Acadia National Park 
-// d3.json("/species").then(data => {
-//     let parkmammalObjs = data.filter(obj1 => {
-
-//         return obj1.park_name === "Acadia National Park" && obj1.category_name === "Mammal";
-
-//     });
-//     console.log(parkmammalObjs);
-// });
-
-// Birds in Acadia National Park 
-// d3.json("/species").then(data => {
-//     let parkbirdObjs = data.filter(obj1 => {
-
-//         return obj1.park_name === "Acadia National Park" && obj1.category_name === "Bird";
-
-//     });
-//     console.log(parkbirdObjs);
-// });
-
-// Reptiles in Acadia National Park 
-// d3.json("/species").then(data => {
-//     let parkreptileObjs = data.filter(obj1 => {
-
-//         return obj1.park_name === "Acadia National Park" && obj1.category_name === "Reptile";
-
-//     });
-//     console.log(parkreptileObjs);
-// });
-
-// Amphibians in Acadia National Park 
-// d3.json("/species").then(data => {
-//     let parkamphObjs = data.filter(obj1 => {
-
-//         return obj1.park_name === "Acadia National Park" && obj1.category_name === "Amphibian";
-
-//     });
-//     console.log(parkamphObjs);
-// });
-
-
-// Highcharts.chart('container', {
-
-//     chart: {
-//         type: 'item'
-//     },
-
-//     title: {
-//         text: 'Acadia National Park'
-//     },
-
-//     subtitle: {
-//         text: 'Animal Classification'
-//     },
-
-//     legend: {
-//         labelFormat: '{name} <span style="opacity: 0.4">{y}</span>'
-//     },
-
-//     series: [{
-//         name: 'Species',
-//         keys: ['name', 'y', 'color', 'label'],
-//         data: [
-//             ['Mammals', 37, '#BE3075', 'MA'],
-//             ['Bird', 214, '#EB001F', 'BRD'],
-//             ['Reptile', 7, '#64A12D', 'REP'],
-//             ['Amphibian', 11, '#FFED00', 'AMPHI']
-//         ],
-//         dataLabels: {
-//             enabled: true,
-//             format: '{point.label}'
-//         },
-
-//         // Circular options
-//         center: ['50%', '88%'],
-//         size: '170%',
-//         startAngle: -100,
-//         endAngle: 100
-//     }]
-// });
-
-
-
